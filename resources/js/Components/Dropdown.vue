ad74fef3f303f537d115739ea0292497715aa7f4
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-01-14 16:32:36 -0800

    chore: wip

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 
 const props = withDefaults(
@@ -11,7 +11,7 @@ const props = withDefaults(
         align: 'right',
         width: '48',
         contentClasses: 'py-1 bg-white',
-    }
+    },
 );
 
 const closeOnEscape = (e: KeyboardEvent) => {
@@ -49,7 +49,11 @@ const open = ref(false);
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false"></div>
+        <div
+            v-show="open"
+            class="fixed inset-0 z-40"
+            @click="open = false"
+        ></div>
 
         <Transition
             enter-active-class="transition ease-out duration-200"
@@ -61,12 +65,15 @@ const open = ref(false);
         >
             <div
                 v-show="open"
-                class="absolute z-50 mt-2 rounded-md shadow-lg"
                 :class="[widthClass, alignmentClasses]"
+                class="absolute z-50 mt-2 rounded-md shadow-lg"
                 style="display: none"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+                <div
+                    :class="contentClasses"
+                    class="rounded-md ring-1 ring-black ring-opacity-5"
+                >
                     <slot name="content" />
                 </div>
             </div>
